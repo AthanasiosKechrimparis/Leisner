f0a6743baa4eb8c4f522df1b695b4e442baa4c78
NicoVill <o_sl_f@hotmail.com>
2015-11-18 10:55:43 +0100

    CreateUSer Prototype (N)

--- LeisnerWebApplication/CreateUserPage.aspx
+++ LeisnerWebApplication/CreateUserPage.aspx
@@ -10,6 +10,23 @@
     <form id="form1" runat="server">
     <div>
     
+        <asp:Label ID="lbl_Name" runat="server" Text="Name"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="txtb_Name" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="lbl_Username" runat="server" Text="Username"></asp:Label>
+&nbsp;<asp:TextBox ID="txtb_Username" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="lbl_Password" runat="server" Text="Password"></asp:Label>
+&nbsp;<asp:TextBox ID="txtb_Password" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="lbl_Device" runat="server" Text="Device"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="txtb_Device" runat="server" OnTextChanged="TextBox4_TextChanged"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="btn_Create" runat="server" OnClick="btn_Create_Click" Text="Create" />
+    
     </div>
     </form>
 </body>
--- LeisnerWebApplication/CreateUserPage.aspx.cs
+++ LeisnerWebApplication/CreateUserPage.aspx.cs
@@ -7,11 +7,40 @@ using System.Web.UI.WebControls;
 
 namespace LeisnerWebApplication
 {
+    class User
+    {
+        public string  name { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public int devicenr { get; set; }
+
+        public User(string n, string u, string p, int d)
+        {
+            name = n;
+            username = u;
+            password = p;
+            devicenr = d;
+        }
+    }
+
     public partial class CreateUserPage : System.Web.UI.Page
     {
+        
+        List<User> listofUser = new List<User>();
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void TextBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_Create_Click(object sender, EventArgs e)
+        {
+            User newUser = new User(txtb_Name.Text, txtb_Username.Text, txtb_Password.Text, int.Parse(txtb_Device.Text));
+            listofUser.Add(newUser);
         }
     }
 }
