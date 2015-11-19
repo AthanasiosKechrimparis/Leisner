cc4d794a13519009dfe7649172516a0e48546354
johann84bjarni@gmail.com <joejoe69>
2015-11-19 12:15:22 +0100

    ViewAccident(J.)

--- LeisnerWebApplication/ViewAccidents.aspx.cs
+++ LeisnerWebApplication/ViewAccidents.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LeisnerWebApplication.LeisnerRef;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,17 +10,25 @@ namespace LeisnerWebApplication
 {
     public partial class ViewAccidents : System.Web.UI.Page
     {
+        BWSServiceClient bedW = new BWSServiceClient();
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if (Session["Admin"] == "NO")
             {
                 IDBox.Enabled = false;
+                IDBox.Text = Session["UserID"].ToString();
+                int ID = Convert.ToInt32(Session["UserID"]);
+                bedW.ReadAccidents(ID);
             }
         }
 
+
         protected void Button1_Click(object sender, EventArgs e)
         {
-
+            bedW.ReadAccidents(int.Parse(IDBox.Text));
+            //ListView1.DataSource = listofAccidents;
+            //ListView1.DataBind();
         }
 
         protected void IDBox_TextChanged(object sender, EventArgs e)
